e87c6a986d81687f155049744318aefa09b55463
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-01-18 14:45:28 +0330

    fix subscribers model bug

--- user/set-user_v2/src/user/user.module.ts
+++ user/set-user_v2/src/user/user.module.ts
@@ -9,6 +9,7 @@ import { EmailService } from 'src/email/email.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { InterconnectionService } from 'src/interconnection/interconnection.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { RabbitMqService } from 'src/rabbit-mq/rabbit-mq.service';
 
 @Module({
   imports : [
@@ -26,6 +27,6 @@ import { MulterModule } from '@nestjs/platform-express';
     })
     , MongooseModule.forRoot('mongodb+srv://kianlucifer0098:lucifer25255225@cluster0.p5b71z1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),MongooseModule.forFeature([{ name: 'user', schema: UserSchema }, { name: 'subscribers', schema: subScribers }])],
   controllers: [UserController],
-  providers: [UserService , TokenService , EmailService , InterconnectionService],
+  providers: [UserService , TokenService , EmailService , InterconnectionService , RabbitMqService],
 })
 export class UserModule {}
